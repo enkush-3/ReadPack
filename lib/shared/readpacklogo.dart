b96687154ea7b80ff0dd3afbea21c892ce9fbc96
enkush-3 <enkush.leymo@gmail.com>
2026-06-21 14:54:56 +0800

    Лого

---
+++ lib/shared/readpacklogo.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ReadPackLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    final colorScheme = Theme.of(context).colorScheme;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 90),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Icon(
+              Icons.auto_stories,
+              color: colorScheme.onPrimary,
+              size: 56,
+            ),
+          ),
+          const SizedBox(width: 12),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Read",
+                style: TextStyle(
+                  fontFamily: 'Serif',
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: colorScheme.onPrimary,
+                  letterSpacing: -1,
+                  height: 1.0,
+                ),
+              ),
+              Text(
+                "Pack",
+                style: TextStyle(
+                  fontFamily: 'Serif',
+                  fontSize: 40,
+                  color: colorScheme.onPrimary,
+                  letterSpacing: -1,
+                  height: 1.0,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+}
