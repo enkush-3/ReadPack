d737fda46af2e25795d7a88f881a07be153717a6
enkush-3 <enkush.leymo@gmail.com>
2026-06-21 14:59:41 +0800

    Text бичиххэсэг

---
+++ lib/shared/widgets/inputfield.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatefulWidget {
+  const InputField({
+    super.key,
+    required this.hint,
+    required this.icon,
+    this.isPassword = false,
+    this.controller,
+  });
+
+  final String hint;
+  final IconData icon;
+  final bool isPassword;
+  final TextEditingController? controller;
+
+  @override
+  State<InputField> createState() => _AuthInputFieldState();
+}
+
+class _AuthInputFieldState extends State<InputField> {
+  bool _obscure = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: colorScheme.surface,
+        borderRadius: BorderRadius.circular(14),
+        border: Border.all(
+          color: colorScheme.outlineVariant,
+          width: 1,
+        ),
+      ),
+      child: TextField(
+        controller: widget.controller,
+        obscureText: widget.isPassword ? _obscure : false,
+        style: TextStyle(
+          fontSize: 15,
+          color: colorScheme.onSurface,
+        ),
+        decoration: InputDecoration(
+          hintText: widget.hint,
+          hintStyle: TextStyle(
+            color: colorScheme.onSurface.withValues(alpha: 0.4),
+            fontSize: 15,
+          ),
+          prefixIcon: Icon(
+            widget.icon,
+            color: colorScheme.onSurface.withValues(alpha: 0.5),
+            size: 20,
+          ),
+          suffixIcon: widget.isPassword
+              ? IconButton(
+            icon: Icon(
+              _obscure
+                  ? Icons.visibility_off_outlined
+                  : Icons.visibility_outlined,
+              color: colorScheme.onSurface.withValues(alpha: 0.5),
+              size: 20,
+            ),
+            onPressed: () {
+              setState(() {
+                _obscure = !_obscure;
+              });
+            },
+          )
+              : null,
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 17,
+            horizontal: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
