51f7aab1a4a460f0c61fc435418aa3347c8c4c4a
enkush-3 <enkush.leymo@gmail.com>
2026-06-21 14:54:28 +0800

    Хэл хооронд солих файл

---
+++ lib/helper/translation.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppLocalizations {
+  final Locale locale;
+  late Map<String, dynamic> _localizedStrings;
+
+  AppLocalizations(this.locale);
+
+  static AppLocalizations? of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+
+  Future<bool> load() async {
+    String jsonString =
+        await rootBundle.loadString('assets/locales/${locale.languageCode}.json');
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+    _localizedStrings = jsonMap.map((key, value) {
+      return MapEntry(key, value.cast<String, dynamic>());
+    });
+
+    return true;
+  }
+
+  String translate(String key) {
+    final parts = key.split('.');
+    dynamic current = _localizedStrings;
+
+    for (final part in parts) {
+      if (current is Map<String, dynamic> && current.containsKey(part)) {
+        current = current[part];
+      } else {
+        return key;
+      }
+    }
+
+    return current.toString();
+  }
+
+
+  String t(String key) => translate(key);
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'mn'].contains(locale.languageCode);
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    final localizations = AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
