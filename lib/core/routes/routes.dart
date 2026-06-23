f8de7a52b640fe1e29b5412f89f08aa7ad670058
enkush-3 <enkush.leymo@gmail.com>
2026-06-23 21:53:27 +0800

    App_shell-ийн route-ийг нэмсэн
    Бусад үдсэн хуудсийг устгасан

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:readpack/app_shell.dart';
 import 'package:readpack/features/auth/presentation/page/auth.dart';
-import 'package:readpack/features/home/presentation/page/home.dart';
 
 class Routes {
   Routes._(_);
 
   static const String auth = '/auth';
   static const String verify = '/verify';
-  static const String home = '/home';
+  static const String appshell = '/shell';
 
   
   static Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case auth:
       return MaterialPageRoute(builder: (_) => AuthPage());
-    case home:
-      return MaterialPageRoute(builder: (_) => HomePage());
-    
-    
-    
+    case appshell:
+      return MaterialPageRoute(builder: (_) => AppShell());
+
+      
     default:
       return MaterialPageRoute(
         builder: (_) => const Scaffold(
