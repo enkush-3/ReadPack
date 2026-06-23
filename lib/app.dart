ecbc6317f3cea4a2f955787f049397ebc1959da8
enkush-3 <enkush.leymo@gmail.com>
2026-06-23 21:52:03 +0800

    App_shell-ийг дууддаг болгосон

--- lib/app.dart
+++ lib/app.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
       darkTheme: BookTheme.darkTheme,
       themeMode: ThemeMode.system,
 
-      initialRoute: Routes.auth,
+      initialRoute: Routes.appshell,
       onGenerateRoute: Routes.generateRoute,
       
     );
