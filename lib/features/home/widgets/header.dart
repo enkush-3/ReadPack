57825ff532cccc6a251e555048a3abbf13f54006
enkush-3 <enkush.leymo@gmail.com>
2026-06-23 21:59:36 +0800

    Notification товчнийн дизайныг өөрчилсөн

--- lib/features/home/widgets/header.dart
+++ lib/features/home/widgets/header.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class PageHeader extends StatelessWidget {
+class HomeHeader extends StatelessWidget {
   final String? userName;
 
-  const PageHeader({
+  const HomeHeader({
     super.key,
     this.userName,
   });
@@ -54,11 +54,20 @@ class PageHeader extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(
+          Container(
             width: 48,
             height: 48,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  colorScheme.primary.withValues(alpha: 0.7),
+                  colorScheme.primary,
+                ],
+              ),
+              shape: BoxShape.circle,
+            ),
             child: IconButton(
-              icon: Icon(Icons.notifications_none_rounded, color: colorScheme.primary),
+              icon: Icon(Icons.notifications_none_rounded, color: colorScheme.onPrimary),
               onPressed: () {},
               //   Navigator.push(
               //     context,
